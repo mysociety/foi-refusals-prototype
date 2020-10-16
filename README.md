2f6184fff53960b46e2c5a0d2cd2723c3c00d3fd
Zarino Zappia <mail@zarino.co.uk>
2020-10-16 15:43:43 +0100

    Proof of concept, form generated from YAML configuration

---
+++ README.md
@@ -0,0 +1,20 @@
+# Building the prototype locally
+
+The prototype is built with Jekyll. If you don’t already have Jekyll installed on your computer, you’ll need:
+
+* a working [Ruby](https://www.ruby-lang.org/en/) environment
+* [Bundler](https://bundler.io/)
+
+You can then install Jekyll (as part of the `github-pages` Gem) by running this, inside the project directory:
+
+    bundle install
+
+With Jekyll installed through Bundler, you can build the site with:
+
+    bundle exec jekyll serve --baseurl ''
+
+If that’s a bit long to type, and you have Make installed (you probably do), then you can use our shortcut:
+
+    make run
+
+Jekyll will process the HTML templates and Sass files, and serve the site locally, at <http://localhost:4000> by default.
---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+var setUpRefusalsWizard = function () {
+    var $form = $(this);
+
+    $('input', $form).on('change', function () {
+        showOrHideDependentQuestions(
+            $(this).attr('name'),
+            $(this).val()
+        );
+    });
+};
+
+var showOrHideDependentQuestions = function (id, value) {
+    var dependent_questions = $('[data-showif-id="' + id + '"]');
+
+    dependent_questions.each(function () {
+        var $this_question = $(this);
+        var show_this_question = false;
+        var showif_operator = $this_question.attr('data-showif-operator');
+        var showif_value = $this_question.attr('data-showif-value');
+        var $options = $this_question.find('input[type="radio"]');
+
+        if ( showif_operator === 'is' ) {
+            show_this_question = ( value === showif_value );
+        } else if ( showif_operator === 'is not' ) {
+            show_this_question = ( value !== showif_value );
+        }
+
+        // Show or hide this question.
+        $this_question.toggleClass( 'd-none', ! show_this_question );
+
+        // If hidden, reset any selections.
+        if ( ! show_this_question ) {
+            $options.prop('checked', false);
+        }
+
+        // Recurse through dependencies, updating visibility and selections.
+        if ( $options.length ) {
+            var option_value = $options.filter(':selected').val() || null;
+            showOrHideDependentQuestions(
+                $this_question.attr('data-question-id'),
+                option_value
+            );
+        }
+    });
+};
+
+$(function () {
+    $('.js-refusals-wizard').each(setUpRefusalsWizard);
+});
